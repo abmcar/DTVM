e9dc28316539d41f9a871bfca592434eaf0e347e
root <2401889661@qq.com>
2025-07-21 09:27:56 +0000

    feat: Add EVMMetricsTableMissing error

--- src/evm/interpreter.cpp
+++ src/evm/interpreter.cpp
@@ -43,12 +43,12 @@ namespace {
 int64_t
 getGasCost(enum evmc_opcode code,
            enum evmc_revision revision = EVMC_SHANGHAI) { // EVMC_SHANGHAI = 11
-  // 获取指定 EVM 版本的指令指标表
+  // Get the instruction index table for the specified EVM version
   const struct evmc_instruction_metrics *metrics_table =
       evmc_get_instruction_metrics_table(revision);
 
   if (metrics_table == nullptr) {
-    return -1;
+    throw common::getError(common::ErrorCode::EVMMetricsTableMissing);
   }
   int16_t gas_cost = metrics_table[code].gas_cost;
   return gas_cost;
